8cfe6f7baf644909f54ff9f9e1e4fb123ca9a6c3
luisdaniel1709 <103487536+luisdaniel1709@users.noreply.github.com>
2022-04-22 13:26:43 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-api_hash = "0c780f6db3124fafbdc3dcde437e6f21"
-api_id = 16536294
-bot_token = "5303873066:AAHoGX4eiQHmmZrOMzM47ROWcQ47lrcfSxY"
-bot_admin_group = -1001505005021
+api_hash = "a5007babf6f3e96c9b51564356b64c30"
+api_id = 11639974
+bot_token = "5287857041:AAFgekXqf2-ppIVH0sesCY7u2zuutqzZZnY"
+bot_admin_group = 1766253419
